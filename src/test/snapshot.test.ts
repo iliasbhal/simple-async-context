b471048032d45bb4636e31151e49a14a7077c675
ilias bhallil <ilias.bhal@gmail.com>
2025-01-17 14:37:23 +0700

    test: add test for snapshot and disposed variable

--- src/test/snapshot.test.ts
+++ src/test/snapshot.test.ts
@@ -46,5 +46,44 @@ describe('AsyncContext / Snapshot', () => {
       expect(asyncContext2.get()).toBe('Inner 2');
     });
   })
+
+  it('should keep have access to data even if variable is disposed', async () => {
+    const asyncContext = new AsyncContext.Variable();
+    const asyncContext2 = new AsyncContext.Variable();
+
+    let snapshot: AsyncSnapshot = null;
+
+    const innerCallback = asyncContext.withData('Inner').wrap(async () => {
+      return asyncContext2.run('Inner 2', async () => {
+        expect(asyncContext.get()).toBe('Inner')
+        await wait(100);
+
+        snapshot = AsyncContext.Snapshot.create()
+        expect(asyncContext.get()).toBe('Inner')
+      })
+    });
+
+    const total = asyncContext.withData('Outer').wrap(async () => {
+      expect(asyncContext.get()).toBe('Outer')
+      await innerCallback();
+      expect(asyncContext.get()).toBe('Outer')
+    });
+
+    await total();
+
+    asyncContext.dispose();
+    asyncContext2.dispose();
+
+    // Ensure we don't have any reference to the original variable
+    AsyncContext.Variable.all.forEach((variable) => {
+      expect(variable).not.toBe(asyncContext);
+      expect(variable).not.toBe(asyncContext2);
+    })
+
+    snapshot.run(() => {
+      expect(asyncContext.get()).toBe('Inner');
+      expect(asyncContext2.get()).toBe('Inner 2');
+    });
+  })
 })
 
